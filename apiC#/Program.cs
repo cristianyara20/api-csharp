805ec9d9de48b7510d62587529b0a8a4e40aae0a
CarlosAcosta44 <carlosacosta12007@gmail.com>
2026-04-24 12:09:30 -0500

    feat: Inject UserRoutes into program

--- apiC#/Program.cs
+++ apiC#/Program.cs
@@ -17,6 +17,12 @@ app.UseCors();
 //PRUEBAS (ruta base)
 app.MapGet("/", () => Results.Json(new { message = "¡Bienvenido a la API REST!", version = "1.0.0" }));
 
+// USERS (Registrar las rutas de usuario)
+app.MapUserRoutes();
+
+// Manejo de errores para rutas no encontradas (404)
+app.MapFallback(() => Results.NotFound(new { error = "Ruta no encontrada" }));
+
 //Manejo de errores para rutas no encontradas (404)
 app.MapFallback(() => Results.NotFound(new { error = "Ruta no encontrada" }));
 
